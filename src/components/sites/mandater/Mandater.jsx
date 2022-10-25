e365c2f678577efccb6d1061c8cadbe0e70ef89b
smallbenji <benjaminkfalch@gmail.com>
2022-10-25 21:00:28 +0200

    updated logos

--- src/components/sites/mandater/Mandater.jsx
+++ src/components/sites/mandater/Mandater.jsx
@@ -3,21 +3,21 @@ import './Mandater.scss';
 import Header from './../../Header/Header'
 
 const data = [
-    { logoPath: "", parti: "Socialdemokratiet", mandater: 0 },
-    { logoPath: "", parti: "Venstre", mandater: 0 },
-    { logoPath: "", parti: "Den Konservative Folkeparti", mandater: 0 },
-    { logoPath: "", parti: "Socialistisk Folkeparti", mandater: 0 },
-    { logoPath: "", parti: "Radikale Venstre", mandater: 0 },
-    { logoPath: "", parti: "Enhedslisten", mandater: 0 },
-    { logoPath: "", parti: "Danmarksdemokraterne", mandater: 0 },
-    { logoPath: "", parti: "Dansk Folkeparti", mandater: 0 },
-    { logoPath: "", parti: "Nye Borgerlige", mandater: 0 },
-    { logoPath: "", parti: "Liberal Alliance", mandater: 0 },
-    { logoPath: "", parti: "Frie Grønne", mandater: 0 },
-    { logoPath: "", parti: "Alternativet", mandater: 0 },
-    { logoPath: "", parti: "Moderaterne", mandater: 0 },
-    { logoPath: "", parti: "Grønlandske mandater", mandater: 0 },
-    { logoPath: "", parti: "Færøske mandater", mandater: 0 },
+    { logoPath: "./pictures/logos/Socialdemokratiet.png", parti: "Socialdemokratiet", mandater: 0 },
+    { logoPath: "./pictures/logos/Venstre.png", parti: "Venstre", mandater: 0 },
+    { logoPath: "./pictures/logos/DetKonservativeFolkeparti.png", parti: "Den Konservative Folkeparti", mandater: 0 },
+    { logoPath: "./pictures/logos/SF.png", parti: "Socialistisk Folkeparti", mandater: 0 },
+    { logoPath: "./pictures/logos/RadikaleVenstre.png", parti: "Radikale Venstre", mandater: 0 },
+    { logoPath: "./pictures/logos/enhedslisten.png", parti: "Enhedslisten", mandater: 0 },
+    { logoPath: "./pictures/logos/Danmarksdemokraterne.png", parti: "Danmarksdemokraterne", mandater: 0 },
+    { logoPath: "./pictures/logos/DF.png", parti: "Dansk Folkeparti", mandater: 0 },
+    { logoPath: "./pictures/logos/NyeBorgerlige.png", parti: "Nye Borgerlige", mandater: 0 },
+    { logoPath: "./pictures/logos/LiberalAlliance.png", parti: "Liberal Alliance", mandater: 0 },
+    { logoPath: "./pictures/logos/Frie Grønne.png", parti: "Frie Grønne", mandater: 0 },
+    { logoPath: "./pictures/logos/Alternativet.png", parti: "Alternativet", mandater: 0 },
+    { logoPath: "./pictures/logos/Moderaterne.png", parti: "Moderaterne", mandater: 0 },
+    { logoPath: "./pictures/logos/grønland.png", parti: "Grønlandske mandater", mandater: 0 },
+    { logoPath: "./pictures/logos/Færøske.jpeg", parti: "Færøske mandater", mandater: 0 },
 ]
 
 export default function Mandater() {
