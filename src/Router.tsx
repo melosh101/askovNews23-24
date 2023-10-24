ddee64581e32d81781a193ffe623c30e4ebdec5d
melosh101 <milasholsting@protonmail.com>
2023-10-24 13:38:36 +0200

    maybe adding it to the router and main page is smart

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StatsSite from "./components/sites/stats/stats";
+import StartSkud from "./components/sites/startSkud/startSkud";
 
 export default function Router() {
     return (
@@ -9,6 +10,7 @@ export default function Router() {
             <Routes>
                 <Route path="/stats" element={<StatsSite />}></Route>
                 <Route path="*" element={<App />} />
+                <Route path="/startSkud" element={<StartSkud />} />
             </Routes>
         </BrowserRouter>
     );
--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./MainPage.scss";
+import BreakingBox from "../breakinBox/BreakingBox";
 
 export default function MainPage() {
-    return <div className="MainPage"></div>;
+    return <div className="MainPage">
+        <BreakingBox 
+            title="START-SKUDET HAR LYDT"
+            color="yellow"
+            sitesrc="/startSkud"
+            breakingText="BREAKING!"
+        />
+    </div>;
 }
 
